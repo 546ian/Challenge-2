07c48959c631d735d2c5f2167e6f61839c0900ab
546ian <ianwaweru38@gmail.com>
2024-09-09 16:14:11 +0300

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, start coroutine to send dog after 2 seconds
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(SpawnDogAfterDelay(1.0f));
+        }
+    }
+
+    // Coroutine to spawn dog after a delay
+    IEnumerator SpawnDogAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+}
